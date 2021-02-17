da5830c3e2d91156637ca4f106dfc700e3679a2a
LuigiTroiano <troiano.luigi8@gmail.com>
2021-02-17 19:44:12 +0100

    Esercizio Completato

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,7 +1,27 @@
-<h1>BLOG</h1>
-<?php
-foreach ($posts as $value) { ?>
-  <p><?php echo $value->title?></p>
-<?php
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog</title>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+
+<body>
+  <h1>BLOG</h1>
+  @foreach ($posts as $value)
+  <div class="container">
+    <div class="box">
+      <span></span>
+      <div class="content">
+        <h2> {{ $value->title }}</h2>
+        <p> {{ $value->body }} </p>
+      </div>
+    </div>
+  </div>
+  @endforeach
+</body>
+
+</html>
